d5afbb4114189934f8255c9706f094fd887765ab
Weijia Li <unicar9@gmail.com>
2017-09-08 15:56:41 +1000

    first commit

---
+++ starbase/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+    <button class="btn btn-primary" @click="fetchCharacter">Test Fetch Character</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        title: 'Generate Your Team'
+      }
+    },
+    methods: {
+      fetchCharacter() {
+        fetch('http://swapi.co/api/people', {
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+      }
+    }
+  }
+</script>
